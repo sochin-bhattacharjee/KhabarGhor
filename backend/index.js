b903af3c52e070fbaaad3d7570736cdfad50c36e
sochin-bhattacharjee <sochin.cs@gmail.com>
2025-11-17 10:19:38 +0600

    Authentication route created and frontend connected to backend using cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 import express from "express"
 import dotenv from "dotenv"
-import connectDB from "./config/db.js";
 dotenv.config()
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js";
+import cors from "cors"
 const app = express()
 const port = process.env.PORT || 3000;
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth", authRouter)
 
 app.listen(port, ()=>{
     connectDB()
