0ae89c30aab93a977f578bbbf4d12576bd8e0fc9
sochin-bhattacharjee <sochin.cs@gmail.com>
2025-11-24 18:56:38 +0600

    I have implemented the Reset Password functionality

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -125,3 +125,22 @@ export const verifyOtp = async (req,res)=>{
         return res.status(500).json(`OTP verify error ${error}`)
     }
 }
+
+// reset password
+export const resetPassword = async (req,res)=>{
+    try {
+        const {email, newPassword} = req.body
+        const user = await User.findOne({email})
+        // check user
+        if (!user || !user.isOtpVerified) {
+            return res.status(400).json({message:"OTP verification required."})
+        }
+        const hashedPassword = await bcrypt.hash(newPassword, 10)
+        user.password=hashedPassword
+        user.isOtpVerified=false
+        await user.save()
+        return res.status(200).json({message:"password reset successfully"})
+    } catch (error) {
+        return res.status(500).json(`Reset password error ${error}`)
+    }
+}
