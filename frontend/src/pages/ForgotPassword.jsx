36ceaa664b7429603e0a066e3de11b7e2765a622
sochin-bhattacharjee <sochin.cs@gmail.com>
2025-11-24 16:51:25 +0600

    I have designed a 3-step UI flow for the Forgot Password process

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -7,6 +7,9 @@ import logo from "../assets/logo.png";
 function ForgotPassword() {
     const [step, setStep] = useState(1)
     const [email, setEmail] = useState("")
+    const [otp, setOtp] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
     const navigate = useNavigate()
   return (
     <div className='bg-gradient-to-br from-yellow-100/20 to-yellow-100/30 flex justify-center items-center min-h-screen'>
@@ -19,6 +22,9 @@ function ForgotPassword() {
             <img src={logo} alt="Logo" className="w-10 md:w-16 h-16 object-contain" />
             <h1 className="text-xl md:text-4xl font-extrabold">Khabar<span className="bg-gradient-to-b from-neutral-300 to-cyan-500 text-transparent bg-clip-text">Ghor</span></h1>
           </div>
+
+            {step == 1 && 
+            <div>
             <div>
           <input
           onChange={(e)=>setEmail(e.target.value)} value={email}
@@ -30,6 +36,49 @@ function ForgotPassword() {
           <button className='w-full p-3 md:p-4 bg-gradient-to-r from-red-400 to-orange-500 text-white rounded-2xl font-semibold shadow-lg hover:scale-105 transition transform cursor-pointer mt-5'>
             Send OTP
           </button>
+          </div>
+            }
+            {step == 2 && 
+            <div>
+            <div>
+          <input
+          onChange={(e)=>setOtp(e.target.value)} value={otp}
+            type="text"
+            placeholder="Enter OTP"
+            className="w-full p-3 md:p-4 rounded-2xl bg-white text-gray-800 outline-none shadow-md placeholder-gray-400 focus:ring-2 focus:ring-orange-300 mt-1"
+          />
+          </div>
+          <button className='w-full p-3 md:p-4 bg-gradient-to-r from-red-400 to-orange-500 text-white rounded-2xl font-semibold shadow-lg hover:scale-105 transition transform cursor-pointer mt-5'>
+            Verify
+          </button>
+          </div>
+            }
+            {step == 3 && 
+            <div>
+            <div className='mb-3'>
+              <label className='text-orange-600 font-bold' htmlFor="">New Password</label>
+          <input
+          onChange={(e)=>setNewPassword(e.target.value)} value={newPassword}
+            type="text"
+            placeholder="Enter New Password"
+            className="w-full p-3 md:p-4 rounded-xl md:rounded-2xl bg-white text-gray-800 outline-none shadow-md placeholder-gray-400 focus:ring-2 focus:ring-orange-300 mt-1"
+          />
+          </div>
+            <div>
+              <label className='text-orange-600 font-bold' htmlFor="">Confirm Password</label>
+          <input
+          onChange={(e)=>setConfirmPassword(e.target.value)} value={confirmPassword}
+            type="text"
+            placeholder="Enter Confirm Password"
+            className="w-full p-3 md:p-4 rounded-xl md:rounded-2xl bg-white text-gray-800 outline-none shadow-md placeholder-gray-400 focus:ring-2 focus:ring-orange-300 mt-1"
+          />
+          </div>
+          <button className='w-full p-3 md:p-4 bg-gradient-to-r from-red-400 to-orange-500 text-white rounded-2xl font-semibold shadow-lg hover:scale-105 transition transform cursor-pointer mt-5'>
+            Reset Password
+          </button>
+          </div>
+            }
+          
         </div>
     </div>
   )
