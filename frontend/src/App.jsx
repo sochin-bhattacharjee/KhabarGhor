f8fdffb6951019cff93359e4efadd02edbc3fc41
sochin-bhattacharjee <sochin.cs@gmail.com>
2025-12-19 10:22:40 +0600

    implement protected and public routes based on user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import {Route, Routes} from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import ForgotPassword from './pages/ForgotPassword'
 import useGetCurrentUser from './hooks/useGetCurrentUser'
-export const serverUrl = "http://localhost:5000"
+import { useSelector } from 'react-redux'
+export const serverUrl = import.meta.env.VITE_SERVER_URL
 function App() {
   useGetCurrentUser()
+  const {userData} = useSelector(state=>state.user)
   return (
     <Routes>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/signin' element={<SignIn/>}/>
-      <Route path='/forgot-password' element={<ForgotPassword/>}/>
+      <Route path='/' element={userData ? <Home/>:<Navigate to={"/signin"} />}/>
+      <Route path='/signup' element={!userData ? <SignUp/> : <Navigate to={"/"}/>}/>
+      <Route path='/signin' element={!userData ? <SignIn/> : <Navigate to={"/"}/>}/>
+      <Route path='/forgot-password' element={!userData ? <ForgotPassword/> : <Navigate to={"/"}/>}/>
     </Routes>
   )
 }
