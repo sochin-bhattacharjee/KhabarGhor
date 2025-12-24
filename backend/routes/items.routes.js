b8c026511511272dd714abec8ef776f37523c9bc
sochin-bhattacharjee <sochin.cs@gmail.com>
2025-12-24 12:51:02 +0600

    add item create and edit APIs with image upload

---
+++ backend/routes/items.routes.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { isAuth } from "../middlewares/isAuth"
+import { addItem, editItem } from "../controllers/item.controllers.js"
+import { upload } from './../middlewares/multer.js';
+
+const itemRouter = express.Router()
+
+itemRouter.post("/add-item",isAuth,upload.single("image"),addItem)
+itemRouter.post("/edit-item",isAuth,upload.single("image"),editItem)
+
+export default itemRouter
